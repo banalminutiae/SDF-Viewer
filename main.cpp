71e1d962120880002d635df152c01ec02cd20b0d
Andrew <andrewli.1920@gmail.com>
2026-03-07 19:04:01 +0800

    Minimal win32 setup

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+
+#include <windows.h>					
+
+LRESULT CALLBACK window_proc(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
+{
+    switch (message)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+
+    return DefWindowProcA(window, message, w_param, l_param);
+}
+
+BOOL init_app(HINSTANCE instance) {
+	WNDCLASS wc = {0};
+	wc.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = window_proc;
+	wc.hInstance = instance;
+	wc.hCursor = LoadCursor (NULL, IDC_ARROW);
+	wc.lpszClassName = "WindowClass";
+
+	return RegisterClass(&wc);	
+}
+
+BOOL init_instance(HINSTANCE instance, INT show_code) {
+	BOOL success = FALSE;
+	HWND window_handle = CreateWindowExA(
+        0,
+        "WindowClass",
+        "app",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        0,
+        0,
+        instance,
+        0
+    );
+
+	if (window_handle) {
+		ShowWindow(window_handle, show_code);
+		success = UpdateWindow(window_handle);
+	}
+	return success;
+}
+
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int show_code) {
+	if (!init_app(instance)) {
+		return FALSE;
+	}
+
+	if (!init_instance(instance, show_code)) {
+		return FALSE;
+	}
+	
+	MSG message;
+	for (;;) {
+		BOOL result = GetMessage(&message, 0, 0, 0);
+		if (result <= 0) {
+			break;
+		}
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+	
+	return 0;
+}
