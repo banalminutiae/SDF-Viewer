70007ca5d6df02c7c3daf439b3028bdf5e94afb8
Andrew <andrewli.1920@gmail.com>
2026-03-07 20:37:37 +0800

    D3D11 swapchain and render target setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "d3dcompiler")
+
+#define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>					
+#include <d3d11.h>
+#include <d3dcompiler.h>
 
 LRESULT CALLBACK window_proc(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
 {
@@ -26,8 +32,11 @@ BOOL init_app(HINSTANCE instance) {
 	return RegisterClass(&wc);	
 }
 
-BOOL init_instance(HINSTANCE instance, INT show_code) {
-	BOOL success = FALSE;
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int show_code) {
+	if (!init_app(instance)) {
+		return FALSE;
+	}
+
 	HWND window_handle = CreateWindowExA(
         0,
         "WindowClass",
@@ -45,20 +54,40 @@ BOOL init_instance(HINSTANCE instance, INT show_code) {
 
 	if (window_handle) {
 		ShowWindow(window_handle, show_code);
-		success = UpdateWindow(window_handle);
-	}
-	return success;
-}
-
-int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int show_code) {
-	if (!init_app(instance)) {
-		return FALSE;
 	}
 
-	if (!init_instance(instance, show_code)) {
-		return FALSE;
-	}
 	
+	//////////////////////////////////////////////////////////////////////////
+
+	D3D_FEATURE_LEVEL featurelevels[] = { D3D_FEATURE_LEVEL_11_0 };
+	
+	DXGI_SWAP_CHAIN_DESC swapchaindesc = {0};
+	swapchaindesc.BufferDesc.RefreshRate.Numerator   = 0;
+	swapchaindesc.BufferDesc.RefreshRate.Denominator = 1;	
+    swapchaindesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapchaindesc.SampleDesc.Count  = 1;
+    swapchaindesc.BufferUsage       = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapchaindesc.BufferCount       = 2;
+    swapchaindesc.OutputWindow      = window_handle;
+    swapchaindesc.Windowed          = TRUE;
+    swapchaindesc.SwapEffect        = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	IDXGISwapChain *swapchain;
+	
+	ID3D11Device *device;
+    ID3D11DeviceContext *devicecontext;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_BGRA_SUPPORT, featurelevels, ARRAYSIZE(featurelevels), D3D11_SDK_VERSION, &swapchaindesc, &swapchain, &device, nullptr, &devicecontext);
+
+	swapchain->GetDesc(&swapchaindesc);
+
+	ID311RenderTargetView* render_target_view_ptr;
+	ID3D11Texture2D *framebuffer;
+
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&framebuffer);
+
+	device->CreateRenderTargetView(framebuffer, nullptr, &render_target_view_tr);
+
 	MSG message;
 	for (;;) {
 		BOOL result = GetMessage(&message, 0, 0, 0);
